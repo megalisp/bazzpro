994ab9d8916d60d4a453b0b657f5e3cf4d7d6f57
( ) MEGA [ie: Jost Grant] <source.megalisp@proton.me>
2025-08-16 13:41:46 -0500

    Add racket-minimal

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,6 @@
 
 set -ouex pipefail
 
-dnf5 install -y rclone restic syncthing git git-lfs git dnf5-plugins --skip-unavailable
+dnf5 install -y racket-minimal rclone restic syncthing git git-lfs git dnf5-plugins --skip-unavailable
 
 systemctl enable podman.socket
