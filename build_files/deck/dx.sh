471143a43d436b2b8886a20d720261ae45284774
( ) MEGA [ie: Jost Grant] <source.megalisp@proton.me>
2025-08-24 06:26:37 -0500

    Continue restructure.

--- build_files/deck/dx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ouex pipefail
-
-# Deck DX-specific build steps can go here
-
-# Source shared steps
-source "$(dirname "$0")/../shared.sh"
---
+++ build_files/desk/devel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Desk DX-specific build steps can go here
+
+# Source shared steps
+source "$(dirname "$0")/../shared.sh"
